0428770c289fbf6c2df0b59d65c5996c61264c0d
kayubin <kayubin@gmail.com>
2025-01-06 22:47:29 +0900

    ✨ 추천 게시글 컴포넌트 생성성

---
+++ src/components/travelPost/travel-post.jsx
@@ -0,0 +1,75 @@
+import * as S from '../../styles/travel-post';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import locationPin from '../../assets/images/locationPin.png';
+import img from '../../assets/images/logo.png';
+
+import image1 from '../../assets/images/image 1.png';
+import image2 from '../../assets/images/image 2.png';
+import image3 from '../../assets/images/image 3.png';
+
+const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+}
+
+//추천 게시글 컴포넌트
+const TravelPost = ({profileSrc, nickname, date, location, travelImages}) => {
+    return (
+        <S.Container>
+            <S.InfoWrapper>
+                <S.Info>
+                    <S.ProfileImg>
+                        <img src={img} alt="프로필" className='profile-img'/>
+                    </S.ProfileImg>
+                    <S.DetailInfo>
+                        <S.TitleDateWrapper>
+                            <div className='nickname'>여행별 일지 콩콩</div>
+                            <div className='date'>2024.09.15</div>
+                        </S.TitleDateWrapper>
+                        <S.LocationWrapper>
+                            <S.LocPin src={locationPin} alt="위치" />
+                            <div>일본, 오사카</div>
+                        </S.LocationWrapper>
+                    </S.DetailInfo>
+                </S.Info>
+                <S.Button type="button">+ 친구 추가</S.Button>
+            </S.InfoWrapper>
+
+            <S.SliderWrapper>
+                <Slider {...settings}>
+                    <S.TravelImg>
+                        <img src={image1} className='travel-img' />
+                    </S.TravelImg>
+                    <S.TravelImg>
+                        <img src={image2} className='travel-img' />
+                    </S.TravelImg>
+                    <S.TravelImg>
+                        <img src={image3} className='travel-img' />
+                    </S.TravelImg>
+                    <S.TravelImg>
+                        <img src={image1} className='travel-img' />
+                    </S.TravelImg>
+                    <S.TravelImg>
+                        <img src={image2} className='travel-img' />
+                    </S.TravelImg>
+                    <S.TravelImg>
+                        <img src={image3} className='travel-img' />
+                    </S.TravelImg>
+                </Slider>
+            </S.SliderWrapper>
+
+            <S.QuickReview>일본 오사카에서 행복했던 여행</S.QuickReview>
+
+            <S.Hr/>
+
+        </S.Container>
+    )
+}
+
+export default TravelPost;
---
+++ src/styles/travel-post.js
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+export const InfoWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Info = styled.div`
+    display: flex;
+    gap: 13px;
+`;
+
+export const DetailInfo = styled.div`
+    display: flex;
+    flex-direction: column; 
+`;
+
+export const TitleDateWrapper = styled.div`
+    display: flex;
+    align-items: flex-end;
+    gap: 5px;
+    margin-bottom: 7px;
+
+    .nickname {
+        font-weight: bold;
+        font-size: 16px;
+        color: #000000;
+    }
+    .date {
+        font-weight: 100;
+        font-size: 13px;
+        color: #000000;
+    }    
+`;
+
+export const LocationWrapper = styled.div`
+    display: flex;
+    gap: 5px;
+    color: #555555;
+`;
+
+export const ProfileImg = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 70%;
+    overflow: hidden;
+
+    .profile-img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export const LocPin = styled.img`
+    height: 13px;
+`;
+
+export const Button = styled.button`
+    width: 121px;
+    height: 34px;
+    border-radius: 15px;
+    background-color: #01BCD4;
+    color: white;
+
+    font-weight: 300;
+    font-size: 16px;
+`;
+
+export const SliderWrapper = styled.div`
+    .slick-list {
+        overflow: hidden; /* 이미지가 슬라이더를 벗어나지 않도록 설정 */
+    }
+
+     .slick-slide {
+        width: 400px !important; /* 슬라이드 항목 너비 강제 설정 */
+        padding-right: 8px;
+    }
+
+    .slick-prev::before,
+    .slick-next::before{
+        color: #9eb23b;
+    }
+`;
+
+export const TravelImg = styled.div`
+    width: 400px;
+    height: 333px;
+    overflow: hidden;
+
+    .travel-img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export const QuickReview = styled.div`
+    font-weight: 100;
+    font-size: 20px;
+    color: #000000;
+`;
+
+export const Hr = styled.hr`
+    width: 100%;
+    border: 0;
+    height: 1px;
+    background-color: #D9D9D9;
+    margin-bottom: 43px;
+`;
