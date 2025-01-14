db451a3d61fd74ad9f668583db42ef7c566782ee
kayubin <kayubin@gmail.com>
2025-01-14 14:48:21 +0900

    ✨ feat: 일지 보기 페이지 생성

--- src/pages/posts/detail.jsx
+++ src/pages/posts/detail.jsx
@@ -1,8 +1,20 @@
+import * as S from '../../styles/detail';
+import TravelPost from '../../components/travelPost/travel-post';
+
 const Detail = () => {
     return (
-        <>
-            detail
-        </>
+        <S.Container>
+            <S.Text>일지 보기</S.Text>
+                <S.Hr/>
+                <S.PostWrapper>
+                    <TravelPost
+                        nickname="벨라" 
+                        date="2024.09.15 14:58" 
+                        location="베트남, 다낭" 
+                        quickReview="바보 원숭이 !! 다낭여행 (2)"
+                        buttonType="edit"  />
+            </S.PostWrapper>
+        </S.Container>
     )
 }
 
---
+++ src/styles/detail.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    margin-top: 2.85vw;
+    width: 71.15vw;
+    height: 100vh;
+    box-sizing: border-box;
+`;
+
+export const Text = styled.div`
+    font-weight: 600;
+    font-size: 1.2vw;
+    color: #555555;
+`;
+
+export const Hr = styled.hr`
+    width: 100%;
+    border: 0;
+    height: 1px;
+    background-color: #D9D9D9;
+`;
+
+export const PostWrapper = styled.div`
+    padding: 1.5vw 0;
+`;
