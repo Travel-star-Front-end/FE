05eaff37796c1675b37200c4a67b4d42537506fb
kayubin <kayubin@gmail.com>
2025-01-21 03:54:58 +0900

    ✨ feat: 친구 보기 tab 생성

---
+++ src/components/travelPost/FriendsTab/friends-tab.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import * as S from '../../../styles/friends-tab';
+import { FriendCard, FriendApplyCard } from "./friend-card";
+
+const FriendsTab = ({ friends, requests }) => {
+    const [activeTab, setActiveTab] = useState("friends");
+
+    return (
+        <S.Container>
+            <S.TabHeader>
+                <button
+                    className={activeTab === "friends" ? "active" : "inactive"}
+                    onClick={() => setActiveTab("friends")}
+                >
+                    친구 목록
+                </button>
+                <button
+                    className={activeTab === "requests" ? "active" : "inactive"}
+                    onClick={() => setActiveTab("requests")}
+                >
+                    친구 신청
+                </button>                    
+            </S.TabHeader>
+            <S.TabContent>
+                {activeTab === "friends" && (
+                <S.CardListContainer>
+                    <FriendCard
+                        name='여행일지'
+                    />
+                    <FriendCard
+                        name='여행일지'
+                    />
+                    <FriendCard
+                        name='여행일지'
+                    />
+                </S.CardListContainer>
+                )}
+                
+                {activeTab === "requests" && (
+                <S.CardListContainer>
+                    <FriendApplyCard
+                        name='젬무'
+                    />
+                    <FriendApplyCard
+                        name='젬무'
+                    />
+                    <FriendApplyCard
+                        name='젬무'
+                    />
+                </S.CardListContainer>
+                )}
+            </S.TabContent>
+        </S.Container>
+    );
+}
+
+export default FriendsTab;
---
+++ src/styles/friends-tab.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    z-index: 100;
+    position: absolute;
+    bottom: 3.1585vw;
+    width: 16.6vw;
+    height: 12vw;
+    border-radius: 8px;
+`;
+
+export const TabHeader = styled.div`
+    display: flex;
+    justify-content: space-around;
+
+    button {
+        background: rgba(123, 123, 123, 0.72);
+        color: #D6D6D6;
+        width: 50%;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+        height: 1.75vw;
+        font-weight: 30vw;
+        font-size: 0.6vw;
+        line-height: 0.726vw;
+
+        &.active {
+            background: rgba(255, 255, 255, 0.6);
+            color: #555555;
+        }
+
+        &.inactive {
+            background: rgba(123, 123, 123, 0.72);
+            color: rgba(214, 214, 214, 1);
+        }
+    }
+`;
+
+export const TabContent = styled.div`
+    height: 10.25vw;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    background-color: rgba(255, 255, 255, 0.6);
+`;
+
+export const CardListContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.8vw;
+    padding-top: 0.5vw;
+`;
