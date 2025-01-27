e943a53e28f8df41e841ae8cba5aa7a54be94088
woojo230 <woojunghyck@naver.com>
2025-01-28 00:54:30 +0900

    다른 사람 행성 보기 기능 개발

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,23 +1,24 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
-import AuthRouter from "./authRouter";
-import RootLayout from "../layouts/root-layout";
-import HomePage from "../pages/home/home";
-import PostsPage from "../pages/posts/posts";
-import PostsDetailPage from "../pages/posts/detail";
-import PostsWritePage from "../pages/posts/write";
-import PostsEditPage from "../pages/posts/edit";
-import PlanetPage from "../pages/planet/planet";
-import RankingPage from "../pages/ranking/ranking";
-import CalenderPage from "../pages/calender/calender";
-import MyPagePage from "../pages/auth/mypage/mypage";
-import MyPageEditPage from "../pages/auth/mypage/edit";
-import MyPagePostsPage from "../pages/auth/mypage/posts";
-import MyPageFriendsPage from "../pages/auth/mypage/friends";
-import NotFoundPage from "../pages/notFound/notFound";
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import AuthRouter from './authRouter';
+import RootLayout from '../layouts/root-layout';
+import HomePage from '../pages/home/home';
+import PostsPage from '../pages/posts/posts';
+import PostsDetailPage from '../pages/posts/detail';
+import PostsWritePage from '../pages/posts/write';
+import PostsEditPage from '../pages/posts/edit';
+import PlanetPage from '../pages/planet/planet';
+import RankingPage from '../pages/ranking/ranking';
+import CalenderPage from '../pages/calender/calender';
+import MyPagePage from '../pages/auth/mypage/mypage';
+import MyPageEditPage from '../pages/auth/mypage/edit';
+import MyPagePostsPage from '../pages/auth/mypage/posts';
+import MyPageFriendsPage from '../pages/auth/mypage/friends';
+import NotFoundPage from '../pages/notFound/notFound';
+import OthersPlanet from '../pages/planet/othersPlanet';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     errorElement: <NotFoundPage />,
     children: [
@@ -26,64 +27,74 @@ const router = createBrowserRouter([
         element: <Navigate to="/login" />,
       },
       {
-        path: "home",
+        path: 'home',
         element: <HomePage />,
       },
 
       {
-        path: "posts",
+        path: 'posts',
         errorElement: <NotFoundPage />,
         children: [
           {
-            index: "true",
+            index: 'true',
             element: <PostsPage />,
           },
           {
-            path: ":id",
+            path: ':id',
             element: <PostsDetailPage />,
           },
           {
-            path: "write",
+            path: 'write',
             element: <PostsWritePage />,
           },
           {
-            path: "edit",
+            path: 'edit',
             element: <PostsEditPage />,
           },
         ],
       },
 
       {
-        path: "planet",
-        element: <PlanetPage />,
+        path: 'planet',
+        errorElement: <NotFoundPage />,
+        children: [
+          {
+            index: 'true',
+            element: <PlanetPage />,
+          },
+          {
+            path: ':id',
+            element: <OthersPlanet />,
+          },
+        ],
       },
       {
-        path: "ranking",
+        path: 'ranking',
         element: <RankingPage />,
       },
       {
-        path: "calender",
+        path: 'calender',
         element: <CalenderPage />,
       },
 
       {
-        path: "mypage",
+        path: 'mypage',
         errorElement: <NotFoundPage />,
         children: [
           {
-            index: "true",
+            index: 'true',
             element: <MyPagePage />,
           },
           {
-            path: "edit",
+            path: 'edit',
             element: <MyPageEditPage />,
           },
           {
-            path: "posts",
+            path: 'posts',
             element: <MyPagePostsPage />,
           },
           {
-            path: "friends",
+            path: 'friends',
             element: <MyPageFriendsPage />,
           },
         ],
