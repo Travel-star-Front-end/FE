db1467e35cac2fe0d0ed79a5a7349a7be2acf827
kayubin <kayubin@gmail.com>
2025-02-05 22:54:38 +0900

    ✨ feat: usePost 생성

---
+++ src/hooks/usePost.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { API } from "../apis/axios";
+
+const usePost = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const triggerPost = async (body = {}) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await API.post(url, body);
+            setData(response.data);
+            return response.data; // 성공 시 데이터 반환
+        } catch (err) {
+            setError(err.message);
+            return null; // 실패 시 null 반환
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { data, loading, error, triggerPost };
+};
+
+export default usePost;
