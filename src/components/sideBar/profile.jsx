4a4f203761c90ab147c1c375671c4f24ba23069a
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-02-07 07:20:33 +0900

    👽 feat: sideBar 행성이름 통신 완료

--- src/components/sideBar/profile.jsx
+++ src/components/sideBar/profile.jsx
@@ -4,20 +4,30 @@ import useFetch from "../../hooks/useFetch";
 
 const Profile = () => {
     const { data, loading, error } = useFetch("/mypage");
-    // console.log(data);
+    const userId = localStorage.getItem("userId");
+    const { data: userData, loading: userLoading } = useFetch(userId ? `/planet/${userId}` : null);
 
     return (
         <s.ProfileContainer>
             <s.ProfileImgContainer>
                 <s.ProfileImg src={ProfilePng} alt="profile" />
             </s.ProfileImgContainer>
-            
+
             <s.PContainer>
-                <s.ProfileP size="1vw" weight="400">{loading ? "Loading..." : data.data.name}</s.ProfileP>
-                <s.ProfileP>{loading ? "Loading..." : data.data.nickname}</s.ProfileP>
+                <s.ProfileP size="1vw" weight="400">
+                    {loading ? "Loading..." : data?.data?.nickname || "닉네임 없음"}
+                </s.ProfileP>
+
+                <s.ProfileP>
+                    {userLoading
+                        ? "Loading..."
+                        : userData?.planet_name
+                        ? `${userData.planet_name} 행성`
+                        : "행성 정보 없음"}
+                </s.ProfileP>
             </s.PContainer>
         </s.ProfileContainer>
-    )
-}
+    );
+};
 
 export default Profile;
