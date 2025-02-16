045e3a7aa5e505526eb6d44a93e5e2a6e9c447d5
kayubin <kayubin@gmail.com>
2025-02-16 20:53:53 +0900

    ✨ feat: home 추천 일지 컴포넌트 생성

---
+++ src/components/home/post-list.jsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import TravelPost from "../posts/posts/travelPost/travel-post";
+import useFetch from "../../hooks/useFetch";
+
+const PostList = () => {
+    const { data: posts, loading: postsLoading, error: postsError } = useFetch(`/home`);
+
+    return (
+        <div>
+            <Text>추천 게시글</Text>
+            <PostWrapper>
+            {/* <TravelPost 
+                key={1}
+                postId={1}
+                postUserId={1}
+                date='2022'
+                location='japan'
+                title='travle of japan'
+                nickname='totoro'
+                buttonType='friend'
+            /> */}
+            {posts?.data.posts.length > 0 ? (
+                <>
+                {posts?.data.posts.map((post) => (
+                    <TravelPost 
+                        key={post.post_id}
+                        postId={post.post_id}
+                        postUserId={post.user_id}
+                        profileImg={post.user.profileImg}
+                        nickname={post.user.nickname}
+                        date={post.updated_at}
+                        location={post.region}
+                        travelImages={post.images}                                
+                        title={post.title}
+                        buttonType='friend'
+                    />
+                ))}                        
+                </>
+                ) : (
+                    <NothingText>추천 게시글이 없습니다.</NothingText>
+                )}
+            </PostWrapper>
+        </div>
+    );
+}
+
+export default PostList;
+
+const Text = styled.div`
+    margin-top: 9vw;
+    font-weight: 600;
+    font-size: 1.2vw;
+    color: #555555;
+`;
+
+const PostWrapper = styled.div`
+   widht: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const NothingText = styled.div`
+    margin-top: 2.75vw;
+    font-weight: 400;
+    font-size: 1vw;
+    color: rgba(85, 85, 85, 0.5);
+`;
