dc8760945947ea3da25cb702b1c32235cf4382bd
kayubin <kayubin@gmail.com>
2025-02-16 20:54:08 +0900

    ✨ feat: 홈 검색 결과 컴포넌트 생성

---
+++ src/components/home/search-travel-post.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import useFetch from "../../hooks/useFetch";
+import useDebounce from "../../hooks/useDebounce";
+import TravelPost from "../posts/posts/travelPost/travel-post";
+
+const SearchTravelPost = ({ searchValue }) => {
+    const url =  `/home/search?term=${encodeURIComponent(searchValue)}`
+    const { data: posts, loading: searchLoading, error: searchError } = useFetch(url);
+
+    if(searchValue && posts.data?.length === 0){
+        <Container>
+            <h3>검색 결과가 없습니다.</h3>
+        </Container>
+    }
+
+    return (
+        <Container>
+            <Text>검색 결과</Text>
+            <Hr/>
+            <PostWrapper>
+                {posts?.data.map((post) => (
+                    <TravelPost 
+                        key={post.post_id}
+                        postId={post.post_id}
+                        postUserId={post.user_id}
+                        profileImg={post.user.profileImg}
+                        nickname={post.user.nickname}
+                        date={post.updated_at}
+                        location={post.region}
+                        travelImages={post.images}                                
+                        title={post.title}
+                        buttonType='friend'
+                    />
+                ))}
+            </PostWrapper>
+        </Container>
+    );
+}
+
+export default SearchTravelPost;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    margin-top: 2.85vw;
+    width: 71.15vw;
+    height: 100vh;
+    box-sizing: border-box;
+`;
+
+const Hr = styled.hr`
+    width: 100%;
+    border: 0;
+    height: 0.05vw;
+    background-color: #D9D9D9;
+`;
+
+const Text = styled.div`
+    font-weight: 600;
+    font-size: 1.2vw;
+    color: #555555;
+`;
+
+const PostWrapper = styled.div`
+    padding: 1.5vw 0;
+`;
