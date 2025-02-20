f9af5d7d8c8ef6ab48e6ba50bc1df9be8618758b
kayubin <kayubin@gmail.com>
2025-02-20 18:13:38 +0900

    📝 docs: 친구 리스트 prop 경로 수정

--- src/components/posts/posts/Tabs/friends-tab.jsx
+++ src/components/posts/posts/Tabs/friends-tab.jsx
@@ -3,23 +3,9 @@ import * as S from '../../../../styles/posts/posts/Tabs/friends-tab';
 import { FriendCard, FriendApplyCard } from "./friend-card";
 import useFetch from "../../../../hooks/useFetch";
 
-//예시 데이터터
-const exampleData = [
-    { id: 1, name: '두구두굳굳' },
-    { id: 2, name: '여행질주' },
-    { id: 3, name: '조이' },
-    { id: 4, name: '오렌지' },
-    { id: 5, name: '여행복한 사람' },
-    { id: 6, name: '히힣ㅎ히힣 일주' },
-    { id: 7, name: 'Hello.K ' },
-    { id: 8, name: '오렌지' },
-    { id: 9, name: 'Hello.K ' },
-    { id: 10, name: '여행질주' },
-];
-
 const FriendsTab = ({ friends, requests }) => {
-    const{data: friendsData, loading, error} = useFetch(`/friends/list`);
-    const{data: applyFriendData, loading: applyFriendLoading, error:applyFriendError } = useFetch(`/friends/list/received`);
+    const{ data: friendsData, loading, error } = useFetch(`/friends/list`);
+    const{ data: applyFriendData, loading: applyFriendLoading, error:applyFriendError } = useFetch(`/friends/list/received`);
 
     const [activeTab, setActiveTab] = useState("friends");
 
@@ -42,14 +28,14 @@ const FriendsTab = ({ friends, requests }) => {
             <S.TabContent>
                 {activeTab === "friends" && (
                 <S.CardListContainer>
-                    {friendsData?.friends?.length > 0 ? (
+                    {friendsData?.data?.length > 0 ? (
                         <>
-                        {friendsData?.map((item) => (
+                        {friendsData?.data?.map((item) => (
                             <FriendCard
-                                key={item.friend_id}
-                                id={item.friend_id}
-                                profileImg={item.profile_image}
-                                name={item.friend_name}
+                                key={item.requestId}
+                                id={item.requestId}
+                                profileImg={item.friendImage?.file_name}
+                                name={item.friendNickname}
                             />
                         ))}
                         </>
